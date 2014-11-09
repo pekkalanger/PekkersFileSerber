b89469fb754d28deaea2a3b16c225f3a3c658b32
emPekka Langer/em <emaxel.nygard@gmail.com/em>
2014-11-09 16:26:45 +0200

    added wwwdir

--- main.go
+++ main.go
@@ -10,24 +10,25 @@ import (
 )
 
 func main() {
-var port string
-    port = "1194"
-    fmt.Println("Port: ")
+    //var port string
+    port := "1194"
+    fmt.Print("Port: ")
     fmt.Scanf("%s", &port)
-    var path string = "./www"
+    var wwwDir string = "./www"
 
     logFile, err := os.OpenFile("server.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
     if err != nil {
         panic(err)
     }
 
-    http.Handle("/", handlers.CombinedLoggingHandler(logFile, http.FileServer(http.Dir(path))))
-    	//http.Handle("/", handlers.CombinedLoggingHandler(os.Stdout, http.FileServer(http.Dir("."))))
-    	//http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(path))))
+    http.Handle("/", handlers.CombinedLoggingHandler(logFile, http.FileServer(http.Dir(wwwDir))))
     addrs, err := net.InterfaceAddrs()
-    if err != nil {   panic(err)}
+    if err != nil { panic(err) }
     for i, addr := range addrs { fmt.Printf("%d %v\n", i, addr) }
-    fmt.Println("Serber running on  " + port);
+
+    serveDir, err := os.Getwd()
+    fmt.Println("Serber running on port " + port);
+    fmt.Println("wwwDir = " + serveDir);
     
     if err := http.ListenAndServe(":" + port, nil); err != nil {
         log.Fatal("ListenAndServe: ", err)
